d175fc51377c145b38d11a20ecee34e290e75e3b
Habibashi <96149736+habibashi@users.noreply.github.com>
2023-07-22 18:24:44 +0300

    views components

---
+++ job-board/resources/views/components/job-card.blade.php
@@ -0,0 +1,27 @@
+<x-card class="mb-4">
+    <div class="mb-4 flex justify-between">
+        <h2 class="text-lg font-medium">{{$job->title}}</h2>
+        <div class="text-slate-500">
+            ${{number_format($job->salary)}}
+        </div>
+    </div>
+    <div class="mb-4 flex justify-between text-sm text-slate-500 items-center">
+        <div class="flex space-x-4">
+            <div>{{$job->employer->company_name}}</div>
+            <div>{{$job->location}}</div>
+        </div>
+        <div class="flex space-x-1 text-xs">
+            <x-tag>
+                <a href="{{route('jobs.index', ['experience' => $job->experience])}}">
+                    {{Str::ucfirst($job->experience)}}
+                </a>
+            </x-tag>
+            <x-tag class="rounded-md border px-2 py-1">
+                <a href="{{route('jobs.index', ['category' => $job->categorys])}}">
+                    {{$job->categorys}}
+                </a>
+            </x-tag>
+        </div>
+    </div>
+    {{$slot}}
+</x-card>
---
+++ job-board/resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Job Board</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="mx-auto mt- max-w-2xl bg-gradient-to-r from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90% text-slate-700">
+    {{$slot}}
+</body>
+</html>
