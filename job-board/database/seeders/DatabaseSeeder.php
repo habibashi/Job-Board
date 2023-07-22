48ed3904e68d741306ae05643f35dedcca078d77
Habibashi <96149736+habibashi@users.noreply.github.com>
2023-07-22 18:23:26 +0300

    migrations and factories and seeds

--- job-board/database/seeders/DatabaseSeeder.php
+++ job-board/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Employer;
+use App\Models\Job;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(300)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $users = User::all()->shuffle();
+
+        for ($i = 0; $i < 20; $i++) {
+            Employer::factory()->create([
+                'user_id' => $users->pop()->id
+            ]);
+        }
+
+        $employers = Employer::all();
+
+        for ($i = 0; $i < 100; $i++) {
+            Job::factory()->create([
+                'employer_id' => $employers->random()->id
+            ]);
+        }
     }
 }
